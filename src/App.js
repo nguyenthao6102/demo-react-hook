cdfafc15add4ce8e8f27354805a813b5332fca5f
thaoxinhtrai77 <nguyenthao6102@gmail.com>
2021-04-22 19:17:17 +0700

    call api render posts and pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import queryString from "query-string";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import { v4 as uuidv4 } from "uuid";
+import PostList from "./components/PostList";
+import Pagination from "./components/Pagination";
 const initialState = [
 	{ id: 1, title: "I love Easy FrontEnd!" },
 	{ id: 2, title: "We love Easy FrontEnd!" },
@@ -12,6 +15,34 @@ const initialState = [
 
 function App() {
 	const [todos, setTodos] = useState(initialState);
+	const [postList, setPostList] = useState([]);
+	const [pagination, setPagination] = useState({
+		_page: 1,
+		_limit: 10,
+		_totalRows: 1,
+	});
+	const [filters, setFilters] = useState({
+		_limit: 10,
+		_page: 1,
+	});
+	useEffect(() => {
+		async function fetchPostList() {
+			try {
+				//_limit=10&_page=1
+				const paramString = queryString.stringify(filters); //convert filter ==> string
+				const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
+				const response = await fetch(requestUrl);
+				const responseJSON = await response.json();
+
+				const { data, pagination } = responseJSON;
+				setPostList(data);
+				setPagination(pagination);
+			} catch (error) {
+				console.log("Failed to fetch post list", error.message);
+			}
+		}
+		fetchPostList();
+	}, [filters]);
 	function handleTodoClick(todo) {
 		const index = todos.findIndex((x) => x.id === todo.id);
 		if (index === -1) {
@@ -30,12 +61,22 @@ function App() {
 		newTodos.push(newTodo);
 		setTodos(newTodos);
 	}
+	function handlePageChange(newPage) {
+		setFilters({
+			...filters,
+			_page: newPage,
+		});
+	}
 	return (
 		<div className="App">
-			<h1>Hello</h1>
+			<h2>Color box change</h2>
 			<ColorBox />
+			<h2>TodoApp (Render, add, delete todo)</h2>
 			<TodoForm onSubmit={handleTodoFormSubmit} />
 			<TodoList todos={todos} onTodoClick={handleTodoClick} />
+			<h2>Post List (Fetch data from api)</h2>
+			<PostList posts={postList} />
+			<Pagination pagination={pagination} onPageChange={handlePageChange} />
 		</div>
 	);
 }
